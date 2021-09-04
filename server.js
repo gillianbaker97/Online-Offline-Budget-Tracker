20c778aeaae4b5ceff65b05d75d2f4564d4cee3b
Gillian Baker <gillianbaker97@gmail.com>
2021-09-03 21:32:25 -0400

    new

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-//const path = require("path");
+const path = require("path");
 
 const PORT = 3000;
 
@@ -21,10 +21,10 @@ mongoose.connect("mongodb://localhost/budget", { useUnifiedTopology: true, useNe
 });
 
 // routes
-// app.use(require("./routes/api.js"));
-// app.use("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./public/index.html"))
-// });
+app.use(require("./routes/api.js"));
+app.use("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"))
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
